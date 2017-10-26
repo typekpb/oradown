e3713ae4d74a6c196a66574cf5822387c983c2f5
Peter Butkovic <butkovic@gmail.com>
2017-10-26 21:26:05 +0200

    test: reflected version change in tests

--- shpec/oradown_shpec.sh
+++ shpec/oradown_shpec.sh
@@ -7,41 +7,41 @@ TEST_URL=http://download.oracle.com/otn/nt/middleware/12c/12212/D-PCT-12212.zip
 
 describe "-V, --version argument"
     it "-V argument exit status is 0"
-        $SHPEC_ROOT/../oradown.sh -V >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh -V >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "--version argument exit status is 0"
-        $SHPEC_ROOT/../oradown.sh --version >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --version >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
-    it "-V prints 'oradown version: 0.0.1'"
+    it "-V prints 'oradown version: 0.0.2'"
         message="$($SHPEC_ROOT/../oradown.sh -V 2>&1)"
-        assert grep "${message}" "oradown version: 0.0.1"
+        assert grep "${message}" "oradown version: 0.0.2"
     end
 
-    it "--version prints 'oradown version: 0.0.1'"
+    it "--version prints 'oradown version: 0.0.2'"
         message="$($SHPEC_ROOT/../oradown.sh --version 2>&1)"
-        assert grep "${message}" "oradown version: 0.0.1"
+        assert grep "${message}" "oradown version: 0.0.2"
     end
 end
 
 describe "-H, --help argument"
     it "-H argument exit status is 0"
-        $SHPEC_ROOT/../oradown.sh -H >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh -H >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "--help argument exit status is 0"
-        $SHPEC_ROOT/../oradown.sh --help >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --help >/dev/null 2>&1
         assert equal "$?" "0"
     end
 end
 
 describe "-P, --password argument"
     it "-P|--password argument missing causes exits status 1"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
@@ -53,7 +53,7 @@ end
 
 describe "-U, --username argument"
     it "-U|--username argument missing causes exits status 1"
-        $SHPEC_ROOT/../oradown.sh --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
@@ -65,7 +65,7 @@ end
 
 describe "C, --cookie argument"
     it "-C|--cookie argument missing causes exits status 1"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} ${TEST_URL} >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
@@ -77,7 +77,7 @@ end
 
 describe "URL"
     it "URL missing causes exits status 1"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
@@ -89,7 +89,7 @@ end
 
 describe "OK input"
     it "OK input causes exits status 0"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} ${TEST_URL} >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
@@ -101,14 +101,14 @@ describe "OK input"
     end
 
     it "OK input with -O argument downloads to the specified file"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} -O /tmp/test.zip ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} -O /tmp/test.zip ${TEST_URL} >/dev/null 2>&1
         assert file_present "/tmp/test.zip"
         # cleanup
         rm -rf /tmp/test.zip
     end
 
     it "OK input with --output argument downloads to the specified file"
-        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} --output=/tmp/test.zip ${TEST_URL} >/dev/null 2>&1 
+        $SHPEC_ROOT/../oradown.sh --username=${TEST_USERNAME} --password=${TEST_PASSWORD} --cookie=${TEST_LICENSE_COOKIE} --output=/tmp/test.zip ${TEST_URL} >/dev/null 2>&1
         assert file_present "/tmp/test.zip"
         # cleanup
         rm -rf /tmp/test.zip
